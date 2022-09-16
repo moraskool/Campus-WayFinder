29f7e25b34fe631198940e7f94e5f263b68fa8d4
Morayo <moraskool@gmail.com>
2022-09-15 17:30:42 -0700

    Edited Splash activity UI

--- app/src/main/java/com/example/campuswayfinder/SplashScreen.java
+++ app/src/main/java/com/example/campuswayfinder/SplashScreen.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
