90891d3aebeec6a17e81399f5fdc17487647e7ae
TanmaySure <chintusure30@gmail.com>
2022-10-01 12:55:32 -0800

    initial commit

---
+++ app/src/main/java/com/example/campus_wayfinder/ByLocation.java
@@ -0,0 +1,14 @@
+package com.example.campus_wayfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ByLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_by_location);
+    }
+}
